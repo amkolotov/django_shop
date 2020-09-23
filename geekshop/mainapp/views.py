ec53d9d8666d40b2dc387a1b94393a50a9ff14f2
Alexander Kolotov <amkolotov@mail.ru>
2020-09-23 22:56:23 +0500

    homework_02

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render
+import json
+
 
 
 def main(request):
-    return render(request, 'mainapp/index.html')
+    return render(request, 'mainapp/index.html', {'title': 'главная'})
 
 
 def products(request):
-    return render(request, 'mainapp/products.html')
+    with open('links.json', 'r') as file:
+        links_menu = json.load(file)
+    context = {'title': 'продукты', 'links_menu': links_menu}
+    return render(request, 'mainapp/products.html', context)
 
 
 def contact(request):
-    return render(request, 'mainapp/contact.html')
+    return render(request, 'mainapp/contact.html', {'title': 'контакты'})
