7f96518ef59df25a49c9e248b61cd5333b85d772
Alexander Kolotov <amkolotov@mail.ru>
2020-09-27 20:15:21 +0500

    homework_03

--- geekshop/mainapp/models.py
+++ geekshop/mainapp/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+
+class ProductCategory(models.Model):
+    name = models.CharField(max_length=64, unique=True, verbose_name='категория')
+    description = models.CharField(max_length=128, blank=True, verbose_name='описание')
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
+    name = models.CharField(max_length=128, verbose_name='имя продукта')
+    image = models.ImageField(upload_to='products_images', blank=True)
+    short_desc = models.CharField(max_length=128, blank=True, verbose_name='краткое описание продукта')
+    description = models.TextField(blank=True, verbose_name='описание продукта')
+    price = models.DecimalField(max_digits=8, decimal_places=2, default=0, verbose_name='цена продукта')
+    quantity = models.PositiveIntegerField(default=0, verbose_name='количество на складе')
+
+    class Meta:
+        verbose_name = 'продукт'
+        verbose_name_plural = 'продукты'
+
+    def __str__(self):
+        return f'{self.name} ({self.category.name})'
+
+
