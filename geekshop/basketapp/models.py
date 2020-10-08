bdca42ebc7e5468e7ea027bd81657b43d14c72a5
Alexander Kolotov <amkolotov@mail.ru>
2020-10-08 10:47:39 +0500

    homework_06

--- geekshop/basketapp/models.py
+++ geekshop/basketapp/models.py
@@ -9,3 +9,19 @@ class Basket(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(default=0, verbose_name='количество')
     add_datetime = models.DateField(auto_now_add=True, verbose_name='время')
+
+    @property
+    def product_cost(self):
+        return self.product.price * self.quantity
+
+    @property
+    def total_quantity(self):
+        _products = Basket.objects.filter(user=self.user)
+        _total_quantity = sum([product.quantity for product in _products])
+        return _total_quantity
+
+    @property
+    def total_cost(self):
+        _products = Basket.objects.filter(user=self.user)
+        _total_cost = sum([product.product_cost for product in _products])
+        return _total_cost
--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -1,27 +1,44 @@
+import random
+
 from django.shortcuts import render, get_object_or_404
 
 from basketapp.models import Basket
 from .models import ProductCategory, Product
 
 
+def get_basket(user):
+    if user.is_authenticated:
+        basket = Basket.objects.filter(user=user)
+    else:
+        basket = []
+    return basket
+
+
+def get_hot_product():
+    products = Product.objects.all()
+    return random.sample(list(products), 1)[0]
+
+
+def get_sample_products(hot_product):
+    sample_products = Product.objects.filter(category=hot_product.category).exclude(pk=hot_product.pk)[:3]
+    return sample_products
+
+
 def main(request):
-    chairs = Product.objects.all()[:4]
+    products = Product.objects.all()[:4]
     title = 'главная'
-    context = {'title': title, 'chairs': chairs}
+    context = {
+        'title': title,
+        'products': products,
+        'basket': get_basket(request.user)
+    }
     return render(request, 'mainapp/index.html', context)
 
 
 def products(request, pk=None):
     title = 'продукты'
     category_menu = ProductCategory.objects.all()
-
-    basket = []
-    basket_quantity = {'quantity': 0, 'cost': 0}
-    if request.user.is_authenticated:
-        basket = Basket.objects.filter(user=request.user)
-        for added_product in basket:
-            basket_quantity['quantity'] += added_product.quantity
-            basket_quantity['cost'] += added_product.product.price * added_product.quantity
+    basket = get_basket(request.user)
 
     if pk is not None:
         if pk == 0:
@@ -37,21 +54,38 @@ def products(request, pk=None):
             'category': category,
             'products': products,
             'basket': basket,
-            'basket_quantity': basket_quantity,
         }
 
         return render(request, 'mainapp/products_list.html', context)
 
-    same_products = Product.objects.all()[3:5]
+    hot_product = get_hot_product()
+    sample_products = get_sample_products(hot_product)
 
     context = {
         'title': title,
         'category_menu': category_menu,
-        'same_products': same_products,
+        'hot_product': hot_product,
+        'sample_products': sample_products,
+        'basket': get_basket(request.user)
     }
 
     return render(request, 'mainapp/products.html', context)
 
+def product(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    sample_products = get_sample_products(product)
+    category_menu = ProductCategory.objects.all()
+
+    context = {
+        'category_menu': category_menu,
+        'product': product,
+        'sample_products': sample_products,
+        'basket': get_basket(request.user)
+    }
+    return render(request, 'mainapp/product.html', context)
+
+
+
 
 def contact(request):
-    return render(request, 'mainapp/contact.html', {'title': 'контакты'})
+    return render(request, 'mainapp/contact.html', {'title': 'контакты', 'basket': get_basket(request.user)})
